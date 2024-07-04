2d00403cdb83968ddeda6caef1770bcc311e1b1e
Shams Eltaf <shams.eltaf786@gmail.com>
2024-07-04 08:09:09 +0000

    08/ 01 Funktion

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -1,29 +1,77 @@
 /*********   CODE CHALLENGE 02 **********/
 
 /*
-// Geben über eine Prompt-Anforderung einen Namen ein
-// Geben über eine Prompt-Anforderung ein Alter ein
+Geben über eine Prompt-Anforderung einen Namen ein
+Geben über eine Prompt-Anforderung ein Alter ein
 
-// Geben Sie in Abhängigkeit vom angegebenen Alter 
-// folgende Getränkeauswahl in die Konsole aus :
+Geben Sie in Abhängigkeit vom angegebenen Alter 
+folgende Getränkeauswahl in die Konsole aus :
 
-// Alter 0-5 // Milch  --> age > 0 AND age < 6
-// Alter 6-12 // Saft
-// Alter 13-17 // Cola
-// 18 + // Wein
+Alter 0-5 // Milch  --> age > 0 AND age < 6
+Alter 6-12 // Saft
+Alter 13-17 // Cola
+18 + // Wein
 
-// -->zB: "Max trinkt Cola."
+-->zB: "Max trinkt Cola."
 
-// /* Getränke-Challenge */
+/* Getränke-Challenge */
 
+// const prompt = require('prompt-sync')({sigint: true});
+
+// let name, alter ;
+
+// name  = prompt("Geben Sie Ihren Namen ein!!: ");
+// alter = parseFloat(prompt("Geben Sie Ihren Alter ein!!! "));
+
+// while (alter<0) {
+//     alter = parseFloat(prompt("Geben Sie Ihren Alter ein: "));
+// }
+// console.log("Datentyp: " + typeof name);
+// console.log("Datentyp: " + typeof alter);
+// console.log(name + " ist " + alter+ " jahre Alt:");
+// if(alter>=0 && alter<=5)
+//     {
+//     console.log(name + " " + "trinkt Milch!!");
+//     }
+// else if (alter>=6 && alter<=12){
+//         console.log(Name +" " + "trinkt Saft!! ");
+//     }
+// else if( alter>=13 && alter<=17)
+//     {
+//         console.log(name + " "+ "trinkt Cola!! ");
+//     }
+// else{
+//     console.log(name + " " + "trinkt wein" );
+// }
+
+//  ++++++++    Switch case ++++++
+
+
+// switch (true) {
+//     case (alter>=0 && alter<=5):
+//         console.log(name + " " + " kann trinkt Milch");
+//         break;
+//     case (alter>= 6 && alter<= 12):
+//         console.log(name + " " + " kann trinkt Saft");
+//         break;
+//     case (alter>=13 && alter<=17):
+//         console.log(name + " " + "kann trinkt Cola");
+//         break;
+    
+//     default:
+//         console.log(name+ " " + " kann trinkt Wein:");
+        
+//         break;
+// }
+         
 const prompt = require('prompt-sync')({sigint: true});
 let firstName = prompt("Vorname?: ");
 let age = parseInt(prompt ("Alter?: "));
 console.log("hier is, " + firstName); 
-console.log(firstName +" is " + (age) + "years old. ");
+console.log(firstName +" ist " + (age) + " jahre alt. ");
 
-// let firstName = "SEA"
-// let age =20
+// let firstName = "Eltaf"
+// let age =30 
 
 const cond = true;
 
