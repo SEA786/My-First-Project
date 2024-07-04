df8c5a35ab3049fc77d490bce0039820e5b9a683
Shams Eltaf <shams.eltaf786@gmail.com>
2024-07-04 09:06:43 +0000

    08 / 2b. Parametrisierung + Datenübergabe von AUSSEN

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -2,7 +2,6 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
-
 // funktionsaufruf | Call 
 // test();
 // function test ()
@@ -21,14 +20,22 @@
 // 2a. Parametrisierung + Datenübergabe von INNEN
 
 // Call
-ausgabeNamen();
-
+// ausgabeNamen();
 // funktion
-
-function ausgabeNamen()
-{
+// function ausgabeNamen()
+// {
    
-   let FirstName = " Eltaf ";
+//    let FirstName = " Eltaf ";
 
+//     console.log("Hallo, " + FirstName + "!");
+// }   
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamen2();
+
+function ausgabeNamen2(FirstName) //parameter
+{
     console.log("Hallo, " + FirstName + "!");
-}   
+}
+
