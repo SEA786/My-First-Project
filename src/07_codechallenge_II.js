2bac65679e78fb786a9a81293fc58a896d92f89a
Shams Eltaf <shams.eltaf786@gmail.com>
2024-07-04 07:53:15 +0000

    07/01 coded

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -16,4 +16,28 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
+const prompt = require('prompt-sync')({sigint: true});
+let firstName = prompt("Vorname?: ");
+let age = parseInt(prompt ("Alter?: "));
+console.log("hier is, " + firstName); 
+console.log(firstName +" is " + (age) + "years old. ");
 
+// let firstName = "sara"
+// let age =20
+
+const cond = true;
+
+switch (cond) {
+    case (0<= age && age <= 5):
+        console.log(firstName+ " " +"can drink milk. ");
+        break;
+    case (6<= age && age <=12):
+        console.log(firstName + " " +"can drink juice. ");
+        break;
+    case(13<= age && age <=17):
+        console.log(firstName + " " +"can drink cola. ")
+        break;
+    default:
+        console.log(firstName + " " + "can drink wein! ");
+        break;
+}
