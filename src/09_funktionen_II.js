d9cf7ecfa3bb2d446ac17ad50a7f361e1705a1c1
Shams Eltaf <shams.eltaf786@gmail.com>
2024-07-05 08:48:41 +0000

    09 / 01  module: division a / b |  test:

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -13,14 +13,53 @@
 
 
 
+// module: addition a + b |  test:
 
 
 
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+
+
+// module: subtraction a - b |  test:
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+// output(subtract(0,2));
+// function subtract(a,b) {
+// 	return a - b;
+// }
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+// function multiply(a,b) {
+// 	return  a * b;
+// }
+
+
+// module: division a / b |  test:
+
+// output( a,b);
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
+output(divide(0,0));
+function divide(a,b) {
+if (b!= 0) {
+	return a / b;
+} else { 
+return"teilen durch 0 nicht möglich";
+}
+}
 
 // module: output | test:
 // output("hello");
 // output(2);
-function output(outputData) {
-	console.log(outputData);
-}
-  
+// function output(outputData) {
+// 	console.log(outputData);
+// }
+
