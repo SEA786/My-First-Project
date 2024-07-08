3bda44c8656b773c7ddee6b2621cb199cd2e61a3
Shams Eltaf <shams.eltaf786@gmail.com>
2024-07-08 08:47:28 +0000

    11 / 04 01a. Funktionalität mit Array 1

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -11,12 +11,12 @@
 // let arr;
 // arr= new Array(); // Konstruktor
 // arr = []; // Literal
-arr = [2,11,7,8,10]; // Array Containter / Element.
+// arr = [2,11,7,8,10]; // Array Containter / Element.
 
-output (arr);
-output(arr.length); // Anzahl der Elemente
-output(arr[0]); // Index 0 (1. Pos. im Array)
-output(arr[arr.length-1]); // letze Pos. / allgemein
+// output (arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[arr.length-1]); // letze Pos. / allgemein
 
 
 
@@ -31,15 +31,49 @@ output(arr[arr.length-1]); // letze Pos. / allgemein
 // --> "Ich bin Max Mütze."
 //  output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4,word5,word6) {
+// function getSentence(word1,word2,word3,word4,word5,word6) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =   word1 + GAP +
+//                 word2 + GAP +
+//                 word3 + GAP +
+//                 word4 + GAP +
+//                 word5 + GAP +
+//                 word6 +
+
+//                 PUNCT;
+
+//     return str;
+// }
+
+
+
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+// function output(outputStr) {
+//     console.log(outputStr);
+// }
+
+
+
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+
+function getSentence(arr) {
     const GAP = " ";
     const PUNCT = ".";
-    let str =   word1 + GAP +
-                word2 + GAP +
-                word3 + GAP +
-                word4 + GAP +
-                word5 + GAP +
-                word6 +
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
 
                 PUNCT;
 
@@ -54,5 +88,3 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 function output(outputStr) {
     console.log(outputStr);
 }
-
-
