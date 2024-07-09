b65aeaf3a84ad6e962c62ab4114b8cac97bbd151
Shams Eltaf <shams.eltaf786@gmail.com>
2024-07-09 08:10:16 +0000

    12 / 01 Test Erledigt

--- src/12_codechallenge_III.js
+++ src/12_codechallenge_III.js
@@ -33,30 +33,6 @@ function getSentence(arr, op) {
 }
 
 
-function getSentence(arr, op) {
-    // Verbinde die Elemente des Arrays zu einem Satz mit Leerzeichen dazwischen
-    let sentence = arr.join(" ");
-    
-    // Füge das entsprechende Satzzeichen basierend auf dem Operator hinzu
-    if (op === "S") {
-        return sentence + ".";
-    } else if (op === "Q") {
-        return sentence + "?";
-    } else if (op === "E") {
-        return sentence + "!";
-    } else {
-        // Für den Fall eines ungültigen Operators
-        return sentence;
-    }
-}
-
-// Tests
-console.log(getSentence(["Ich", "bin", "Max"], "S")); // Ich bin Max.
-console.log(getSentence(["Bist", "du", "Max"], "Q")); // Bist du Max?
-console.log(getSentence(["Ich", "bin"], "E")); // Ich bin!
-console.log(getSentence(["Ich", "hätt", "gern", "die", "Platt", "von", "dene", "zwei", "diwodaso", "Spass", "mache,", "habbe", "Sie", "die"], "Q")); // Ich hätt gern die Platt von dene zwei diwodaso Spass mache, habbe Sie die?
-
-
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
 function output(outputData) {
